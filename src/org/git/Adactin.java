7895d9d1e3ca4b791b8b69290ff55c471f8b7e9f
sengathir96 <sengathir100@gmail.com>
2021-11-09 17:55:24 +0530

    conflict resolved

--- src/org/git/Adactin.java
+++ src/org/git/Adactin.java
@@ -2,12 +2,12 @@ package org.git;
 
 public class Adactin {
 
-	public static void print1() {
-		System.out.println("PassWord");
-	}
-
 	public static void print() {
 		System.out.println("UserName");
+	}
+
+	public static void print1() {
+		System.out.println("PassWord");
 	}
 
 	public static void main(String[] args) {
